ea685a0a7d3410e00e6b6652def6887900f7c247
Marat Mikaelyan <MMikaelyan@alfabank.ru>
2019-10-01 15:34:37 +0300

    Move ColorScheme into separate file

--- Snake/Snake/GameScene.swift
+++ Snake/Snake/GameScene.swift
@@ -141,37 +141,3 @@ struct CollisionCategories{
     // Край сцены (экрана)
     static let EdgeBody: UInt32 = 0x1 << 3
 }
-
-struct ColorScheme {
-    var background: SKColor
-    var apple: UIColor
-    var snake: UIColor
-    
-    init(background: SKColor, apple: UIColor, snake: UIColor) {
-        self.background = background
-        self.apple = apple
-        self.snake = snake
-    }
-}
-
-let darkColorScheme = ColorScheme(background: SKColor.black, apple: UIColor.red, snake: UIColor.green)
-let lightColorScheme = ColorScheme(background: SKColor.white, apple: UIColor.yellow, snake: UIColor.blue)
-
-enum ColorSchemeSwitch {
-    case dark
-    case light
-    case defaultScheme
-    
-    var scheme: ColorScheme {
-        switch self {
-        case .dark:
-            return darkColorScheme
-        case .light:
-            return lightColorScheme
-        case .defaultScheme:
-            return lightColorScheme
-        }
-    }
-}
-
-
